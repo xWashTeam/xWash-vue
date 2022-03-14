09552b8aea3fab166c448a2b5e9dc2e0abf8fc35
wulnm <wumqing@yeah.net>
2022-03-14 21:48:38 +0800

    fix: fix vuetify exception

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -7,6 +6,5 @@ import store from './store'
 const app = createApp(App)
 app.use(router)
 app.use(store)
-app.use(vuetify)
 
 app.mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     hostLink: "https://xwash.one/",
-    apiLink: process.env.NODE_ENV=="production"?"https://xwash.one/api/":"http://localhost:8082/xWash_war/api/",
+    apiLink: process.env.NODE_ENV=="production"?"https://xwash.one/api/":"https://xwash.one/api/",
     feedbackLink : "https://xwash.one/feedback"
   },
   mutations: {},
