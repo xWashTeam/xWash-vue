4aa25b896c0c39cfaa54629f06512029c5e34fbc
齐 <48374300+wulnm@users.noreply.github.com>
2022-03-14 15:29:00 +0800

    feat: change http to https

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,9 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    hostLink: "http://xwash.one/",
-    apiLink: process.env.NODE_ENV=="production"?"http://xwash.one/api/":"http://localhost:8082/xWash_war/api/",
-    feedbackLink : "http://xwash.one/feedback"
+    hostLink: "https://xwash.one/",
+    apiLink: process.env.NODE_ENV=="production"?"https://xwash.one/api/":"http://localhost:8082/xWash_war/api/",
+    feedbackLink : "https://xwash.one/feedback"
   },
   mutations: {},
   actions: {},
